c9b358287f225e5bda96ff42a5eb8ee94e30d2af
ying <ying285@hotmail.com>
2023-02-02 17:35:55 +0100

    essage2

--- component/content/ContentText.tsx
+++ component/content/ContentText.tsx
@@ -12,10 +12,12 @@ const ContentText = () => {
     
     <div className={styles.contentText_detail}>
       <Slide direction='up'cascade>
+    <div>
     <p>I am a frontend developer in Stockholm, Sweden.</p>
     <p>Now I am looking for a new opportunity to continue</p>
     <p>my career as frontend developer.</p>
     <p>Contact me if you are interested in my experiences.</p>
+    </div>
     </Slide>
      </div>
      
