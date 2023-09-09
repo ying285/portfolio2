d05d2c9fae0ce56c5aec0a209613e7819442db9b
ying <ying285@hotmail.com>
2023-09-09 07:49:54 +0200

    change11

--- component/content/Data.tsx
+++ component/content/Data.tsx
@@ -1,9 +1,13 @@
 export const detail0:any = <><p>Work as freelancer.</p> </>
 
-export const detail1:any = <><p>Work as project manager and frontend developer.</p> <p>Cooperate with an outsourcing company</p> 
-<p> to develop an APP and a website.</p> </>
+export const detail1:any = <>
+<p>Worked as frontend developer. Part of the.</p> 
+<p>responsibilities was  to work as a project manager</p> 
+<p>here I was responsible for monitoring and communicating</p>
+<p>with third-party developer for a big project.</p>
+ </>
 
-export const detail2:any= <><p>Work as frontend developer, builds a two-sided</p> <p>marketing platform for buyers and sellers to exchange</p><p>value products in the field of pleasing activities</p> </>
+export const detail2:any= <><p>Worked as frontend developer, built a two-sided</p> <p>marketing platform for buyers and sellers to exchange</p><p>value products in the field of pleasing activities</p> </>
 
 export const detail3:any= <><p>A frontend course in Stockholm, Sweden</p> </>
 
